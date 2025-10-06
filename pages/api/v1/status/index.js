4df5dcb38ee0278154ae4b5acf750bfa3652fbfe
Riobis <riobisofc@gmail.com>
2025-10-06 20:16:02 +0000

    add 'database.js' and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
-  response.status(200).json({mensagem: "valor"})
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows)
+  response.status(200).json({ mensagem: "valor" });
 }
-export default status
+export default status;

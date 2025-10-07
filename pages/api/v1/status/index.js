51c64efdf3b0e328d088804362e47d46c281e57d
Riobis <riobisofc@gmail.com>
2025-10-07 17:26:01 +0000

    add queries

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,31 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows)
-  response.status(200).json({ mensagem: "valor" });
+  const updatedAt = new Date().toISOString()
+
+  const databaseVersion = await database.query("SHOW server_version;")
+  const databaseVersionValue = databaseVersion.rows[0].server_version
+
+  const maxConnection = await database.query("SHOW max_connections;")
+  const maxConnectionValue = maxConnection.rows[0].max_connections
+
+  const databaseOpenedConnectionResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';"
+  )
+
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionResult.rows.length
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
+  })
 }
-export default status;
+
+export default status

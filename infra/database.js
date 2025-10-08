52ac58a9ad13d22cb415e6ff4b2569500077184d
Riobis <riobisofc@gmail.com>
2025-10-08 20:01:38 +0000

    add migrations scripts

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,6 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   })
-  console.log("credencias do postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-
   try {
     await client.connect()
     const result = await client.query(queryObject)
